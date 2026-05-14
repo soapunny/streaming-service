f8241f8f69240ffefaa20cdb5c65dc931491a8ae
soapunny <soapunny@gmail.com>
2026-05-14 19:49:06 -0400

    chore: migrate project to TypeScript
    - remove @types/react-router-dom (built-in types in v7)
    - add @types/node for node:url support
    - replace jsconfig.json with tsconfig.json
    - rename vite.config.js to vite.config.ts
    - rename main.jsx/App.jsx to tsx

---
+++ client/src/App.tsx
@@ -0,0 +1,29 @@
+// client/src/App.tsx
+
+import '@/global.css'
+import { Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import { MovieDetailsScreen } from './screens/MovieDetailScreen';
+import NavBar from './components/layout/Navbar';
+import SearchScreen from './screens/SearchScreen';
+// import EffectsTest from '@/test/effects'
+// import CleanupTest from './test/cleanup';
+// import TodoList from './test/todo-list';
+// import CryptoTracker from './test/crypto-tracker';
+
+function App() {
+  return (
+    <div className='main-wrapper'>
+      <NavBar />
+      <main className='main-content'>
+        <Routes>
+          <Route path="/" element={<HomeScreen />} />
+          <Route path="/movie/:id" element={<MovieDetailsScreen />} />
+          <Route path="/search/:keyword" element={<SearchScreen />} />
+        </Routes>
+      </main>
+    </div>
+  );
+}
+
+export default App
---
+++ client/src/main.tsx
@@ -0,0 +1,18 @@
+// client/src/main.tsx
+
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App'
+import { BrowserRouter } from 'react-router-dom'
+
+const rootElement = document.getElementById('root') //return HTMLElement or null
+if (!rootElement) throw new Error('Cannot find the root element') //throw error if root element is 'null'
+
+createRoot(rootElement).render(//rootElement is not null, so it is only HTMLElement
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+)
